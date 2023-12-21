651393e69e2141fa43992d6cc8a3e7e3565a98e1
Muqimjon <muqimjon.mr@gmail.com>
2023-12-21 22:35:48 +0500

    filled investment application

--- src/OrgBloom.Domain/Entities/Investment/InvestmentApp.cs
+++ src/OrgBloom.Domain/Entities/Investment/InvestmentApp.cs
@@ -1,5 +1,13 @@
 ﻿namespace OrgBloom.Domain.Entities.Investment;
 
-public class InvestmentApp
+public class InvestmentApp : Auditable
 {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTimeOffset DateOfBirth { get; set; }
+    public string Degree { get; set; } = string.Empty;
+    public string Sector { get; set; } = string.Empty;
+    public string InvestmentAmount { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
