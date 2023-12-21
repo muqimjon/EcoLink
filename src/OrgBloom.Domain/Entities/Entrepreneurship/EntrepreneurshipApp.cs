62cc7939c715278395e1ab52a12206d7bd9dabe5
Muqimjon <muqimjon.mr@gmail.com>
2023-12-21 22:36:15 +0500

    filled entrepreneurship application

--- src/OrgBloom.Domain/Entities/Entrepreneurship/EntrepreneurshipApp.cs
+++ src/OrgBloom.Domain/Entities/Entrepreneurship/EntrepreneurshipApp.cs
@@ -2,4 +2,15 @@
 
 public class EntrepreneurshipApp
 {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTimeOffset? DateOfBirth { get; set; }
+    public string Degree { get; set; } = string.Empty;
+    public string Experience { get; set; } = string.Empty;
+    public string Project { get; set; } = string.Empty;
+    public string HelpType { get; set; } = string.Empty;
+    public string RequiredFunding { get; set; } = string.Empty;
+    public string AssetsInvested { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
