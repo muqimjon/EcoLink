a1d3a8bfd40aad4d63b66aa2358254ba8ec77168
Muqimjon <muqimjon.mr@gmail.com>
2023-12-21 22:45:26 +0500

    filled project management application

--- src/OrgBloom.Domain/Entities/Entrepreneurship/EntrepreneurshipApp.cs
+++ src/OrgBloom.Domain/Entities/Entrepreneurship/EntrepreneurshipApp.cs
@@ -4,7 +4,7 @@ public class EntrepreneurshipApp
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public DateTimeOffset? DateOfBirth { get; set; }
+    public DateTimeOffset DateOfBirth { get; set; }
     public string Degree { get; set; } = string.Empty;
     public string Experience { get; set; } = string.Empty;
     public string Project { get; set; } = string.Empty;
