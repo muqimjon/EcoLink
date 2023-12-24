004d3bfb12b3e46f0028f0521f3f133bc8e1f12f
Muqimjon <muqimjon.mr@gmail.com>
2023-12-24 17:29:42 +0500

    Project management app sheets and Representation app sheets dtos added

---
+++ src/OrgBloom.Application/ProjectManagementApps/DTOs/ProjectManagedmentAppSheetsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace OrgBloom.Application.ProjectManagementApps.DTOs;
+
+public class ProjectManagedmentAppSheetsDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Age { get; set; } = string.Empty;
+    public string Degree { get; set; } = string.Empty;
+    public string Languages { get; set; } = string.Empty;
+    public string Experience { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string ProjectDirection { get; set; } = string.Empty;
+    public string Expectation { get; set; } = string.Empty;
+    public string Purpose { get; set; } = string.Empty;
+    public string WasCreated { get; set; } = string.Empty;
+}
---
+++ src/OrgBloom.Application/RepresentationApps/DTOs/RepresentationAppSheetsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace OrgBloom.Application.RepresentationApps.DTOs;
+
+public class RepresentationAppSheetsDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Patronomyc { get; set; } = string.Empty;
+    public string Age { get; set; } = string.Empty;
+    public string Degree { get; set; } = string.Empty;
+    public string Languages { get; set; } = string.Empty;
+    public string Experience { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string Area { get; set; } = string.Empty;
+    public string Expectation { get; set; } = string.Empty;
+    public string Purpose { get; set; } = string.Empty;
+    public string WasCreated { get; set; } = string.Empty;
+}
