afd7065f1df086bf585c2b3509663710d686a6ee
Muqimjon <muqimjon.mr@gmail.com>
2023-12-24 17:12:07 +0500

    Entrepreneur app for Sheets

---
+++ src/OrgBloom.Application/EntrepreneurshipApps/DTOs/EntrepreneurshipAppForSheetsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace OrgBloom.Application.EntrepreneurshipApps.DTOs;
+
+public class EntrepreneurshipAppForSheetsDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string DateOfBirth { get; set; } = string.Empty;
+    public string Degree { get; set; } = string.Empty;
+    public string Experience { get; set; } = string.Empty;
+    public string Project { get; set; } = string.Empty;
+    public string HelpType { get; set; } = string.Empty;
+    public string RequiredFunding { get; set; } = string.Empty;
+    public string AssetsInvested { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string MyProperty { get; set; } = string.Empty;
+}
