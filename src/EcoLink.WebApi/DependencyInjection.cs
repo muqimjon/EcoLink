d26c3ee64246ce420d8a21b89cb9a51ba4fa954c
Muqimjon <muqimjon.mr@gmail.com>
2024-01-19 00:47:12 +0500

    🚀 Added Deploy job

---
+++ src/EcoLink.WebApi/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Auth.OAuth2;
+using EcoLink.Infrastructure.Models;
+
+namespace EcoLink.WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddThis(this IServiceCollection services, 
+        GoogleAuthSettings googleAuthSettings,
+        IConfiguration configuration)
+    {
+        var g = JsonConvert.SerializeObject(googleAuthSettings);
+
+        // Add Sheets configure
+        services.AddSingleton(new SheetsConfigure()
+        {
+            SpreadsheetId = configuration.GetConnectionString("SpreadsheetId")!,
+            Service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = GoogleCredential.FromJson(json: g),
+                ApplicationName = configuration.GetValue("ApplicationName", string.Empty),
+            }),
+        });
+
+        // Add localization
+        services.AddLocalization();
+        return services;
+    }
+}
