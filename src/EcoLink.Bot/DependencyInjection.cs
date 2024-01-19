5e2888915732c8d024241a4d361fccc4638332ca
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-01-19 19:25:37 +0500

    🔄 Updated confirmation info retrieval for Google auth

--- src/EcoLink.Bot/DependencyInjection.cs
+++ src/EcoLink.Bot/DependencyInjection.cs
@@ -11,9 +11,7 @@ namespace EcoLink.Bot;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddThis(this IServiceCollection services, 
-        GoogleAuthSettings googleAuthSettings,
-        IConfiguration configuration)
+    public static IServiceCollection AddThis(this IServiceCollection services, IConfiguration configuration)
     {
         // Get bot token
         var token = configuration.GetConnectionString("BotToken");
@@ -23,18 +21,30 @@ public static class DependencyInjection
         services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
         services.AddHostedService<BotBackgroundService>();
 
-        var g = JsonConvert.SerializeObject(googleAuthSettings);
+        // Add sheets service
+        #region Add Sheets configure
+        #region Google Auth Settings converting to json
+        GoogleAuthSettings googleAuth = new();
 
-        // Add Sheets configure
+        var properties = typeof(GoogleAuthSettings).GetProperties();
+        foreach (var property in properties)
+            property.SetValue(googleAuth, 
+                configuration.GetValue(property.Name, 
+                    property.GetValue(googleAuth)));
+
+        var googleAuthJson = JsonConvert.SerializeObject(googleAuth);
+        #endregion
+
         services.AddSingleton(new SheetsConfigure()
         {
             SpreadsheetId = configuration.GetConnectionString("SpreadsheetId")!,
             Service = new SheetsService(new BaseClientService.Initializer()
             {
-                HttpClientInitializer = GoogleCredential.FromJson(json: g),
+                HttpClientInitializer = GoogleCredential.FromJson(json: googleAuthJson),
                 ApplicationName = configuration.GetValue("ApplicationName", string.Empty),
             }),
         });
+        #endregion
 
         // Add localization
         services.AddLocalization();
