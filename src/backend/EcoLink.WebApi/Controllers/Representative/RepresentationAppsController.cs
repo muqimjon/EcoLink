319c80803dee0a52ccab41e4ec64cc2926277b2f
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-01-23 11:33:39 +0500

    👨‍💼 Representation controller completed

---
+++ src/backend/EcoLink.WebApi/Controllers/Representative/RepresentationAppsController.cs
@@ -0,0 +1,23 @@
+﻿using EcoLink.Domain.Entities.Representation;
+using EcoLink.Application.Representatives.Queries.GetRepresentatives;
+using EcoLink.Application.RepresentationApps.Commands.CreateRepresentationApps;
+
+namespace EcoLink.WebApi.Controllers.Representative;
+
+public class RepresentationAppsController(IMediator mediator) : BaseController
+{
+    [HttpPost("create-with-return")]
+    [ProducesResponseType(typeof(RepresentationApp), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Create(CreateRepresentationAppWithReturnCommand command)
+        => Ok(await mediator.Send(new CreateRepresentationAppWithReturnCommand(command)));
+
+    [HttpGet("get/{id:long}")]
+    [ProducesResponseType(typeof(RepresentationApp), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Get([FromQuery] GetRepresentativeQuery query)
+        => Ok(await mediator.Send(new GetRepresentativeQuery(query)));
+
+    [HttpGet("get-all-by-user-id")]
+    [ProducesResponseType(typeof(IEnumerable<RepresentationApp>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllByUserId([FromQuery] GetRepresentativeQuery query)
+        => Ok(await mediator.Send(new GetRepresentativeQuery(query)));
+}
--- src/backend/EcoLink.WebApi/Controllers/Representative/RepresentativeController.cs
+++ src/backend/EcoLink.WebApi/Controllers/Representative/RepresentativeController.cs
@@ -1,4 +1,5 @@
-﻿using EcoLink.Application.Representatives.Queries.GetRepresentatives;
+﻿using EcoLink.Application.Representatives.DTOs;
+using EcoLink.Application.Representatives.Queries.GetRepresentatives;
 using EcoLink.Application.Representatives.Commands.CreateRepresentatives;
 using EcoLink.Application.Representatives.Commands.DeleteRepresentatives;
 using EcoLink.Application.Representatives.Commands.UpdateRepresentatives;
@@ -8,22 +9,36 @@ namespace EcoLink.WebApi.Controllers.Representative;
 public class RepresentativesController(IMediator mediator) : BaseController
 {
     [HttpPost("create")]
+    [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
     public async Task<IActionResult> Create(CreateRepresentativeCommand command)
         => Ok(await mediator.Send(new CreateRepresentativeCommand(command)));
 
+    [HttpPost("create-with-return")]
+    [ProducesResponseType(type: typeof(RepresentativeResultDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> CreateWithReturn(CreateRepresentativeWithReturnCommand command)
+        => Ok(await mediator.Send(new CreateRepresentativeWithReturnCommand(command)));
+
     [HttpPut("update")]
+    [ProducesResponseType(typeof(RepresentativeResultDto), StatusCodes.Status200OK)]
     public async Task<IActionResult> Update(UpdateRepresentativeCommand command)
         => Ok(await mediator.Send(new UpdateRepresentativeCommand(command)));
 
-    [HttpDelete("delete")]
-    public async Task<IActionResult> Delete(DeleteRepresentativeCommand command)
+    [HttpDelete("delete/{id:long}")]
+    public async Task<IActionResult> Delete([FromQuery] DeleteRepresentativeCommand command)
         => Ok(await mediator.Send(new DeleteRepresentativeCommand(command)));
 
-    [HttpGet("get")]
+    [HttpGet("get/{id:long}")]
+    [ProducesResponseType(typeof(RepresentativeResultDto), StatusCodes.Status200OK)]
     public async Task<IActionResult> Get([FromQuery] GetRepresentativeQuery command)
         => Ok(await mediator.Send(new GetRepresentativeQuery(command)));
 
+    [HttpGet("get-by-user-id/{user-id:long}")]
+    [ProducesResponseType(typeof(IEnumerable<RepresentativeResultDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetByUserId([FromQuery] GetRepresentativeQuery command)
+        => Ok(await mediator.Send(new GetRepresentativeQuery(command)));
+
     [HttpGet("get-all")]
+    [ProducesResponseType(typeof(IEnumerable<RepresentativeResultDto>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll()
         => Ok(await mediator.Send(new GetAllRepresentativesQuery()));
 }
